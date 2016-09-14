fcb7b87f9cecebf2ffb45655ec4f9553d32b5d9c
fred belotte <fred@revature.com>
2016-09-14 09:23:30 -0400

    add the basic stages for the pipeline.

---
+++ jenkins-dotnet.groovy
@@ -0,0 +1,31 @@
+// jenkins pipeline :: dotnet
+
+stage("import") {
+  node() {
+
+  }
+}
+
+stage("analyze") {
+  node() {
+
+  }
+}
+
+stage("test") {
+  node() {
+
+  }
+}
+
+stage("deploy") {
+  node() {
+
+  }
+}
+
+stage("export") {
+  node() {
+
+  }
+}
