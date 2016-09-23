c0a5055a8cc8860a51aae797dbe50ee7b89be00f
fred belotte <fred@revature.com>
2016-09-23 18:03:53 -0400

    add the slack notification directive.

--- jenkins/jenkins-nodejs.groovy
+++ jenkins/jenkins-nodejs.groovy
@@ -1,7 +1,25 @@
 // jenkins pipeline :: nodejs
 
+def buildColor = [green: "#00AA00", red: "#AA0000"]
+def buildFlag = [failing: "FAIL", passing: "PASS"]
 def gitBranch = "**"
 def gitUrl = "https://github.com/<account-name>/<repository-name>.git"
+def slackChannel = "#<channel>"
+
+def slackNotify(buildChannel, buildColor, buildStage, buildFlag) {
+  def author = bat returnStdout: true, script: "git log -1 --pretty=%%an"
+  def branch = bat returnStdout: true, script: "git rev-parse --abbrev-ref=strict head"
+  def commit = bat returnStdout: true, script: "git log -1 --pretty=\"%%s - %%h\""
+  
+  author = (author.split("\n"))[2]
+  branch = (branch.split("\n"))[2]
+  commit = (commit.split("\n"))[2]
+  
+  messageHead = "${env.JOB_NAME} - ${env.BUILD_NUMBER} - ${buildStage} - ${buildFlag}"
+  messageBody = "${author} - ${branch} - ${commit}"
+  
+  slackSend channel: buildChannel, color: buildColor, message: "${messageHead}\n${messageBody}"
+}
 
 stage("IMPORT") {
   node() {
