82d65dd69a140a17088592e3ba183e6eb8512c38
fred belotte <fred@revature.com>
2016-09-23 17:35:19 -0400

    add the nodejs jenkins pipeline script.

---
+++ jenkins/jenkins-nodejs.groovy
@@ -0,0 +1,49 @@
+// jenkins pipeline :: nodejs
+
+stage("IMPORT") {
+  node() {
+    try {
+
+    } catch(error) {
+      throw error
+    }
+  }
+}
+
+stage("ANALYZE") {
+  node() {
+    try {
+    } catch(error) {
+      throw error
+    }
+  }
+}
+
+stage("TEST") {
+  node() {
+    try {
+    } catch(error) {
+      throw error
+    }
+  }
+}
+
+stage("DEPLOY") {
+  node() {
+    try {
+
+    } catch(error) {
+      throw error
+    }
+  }
+}
+
+stage("EXPORT") {
+  node() {
+    try {
+
+    } catch(error) {
+      throw error
+    }
+  }
+}
